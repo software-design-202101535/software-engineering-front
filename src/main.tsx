8fc6b8ca4a9f7145f7166cdd52760f4710b9908d
Jun-77 <ghkdqhdmswns@naver.com>
2026-05-26 17:52:01 +0900

    feat: foreground 푸시 수신 시 sonner 토스트 표시

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'sonner'
 import './index.css'
 import App from './App'
 
@@ -20,6 +21,7 @@ createRoot(root).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Toaster position="top-right" richColors closeButton />
     </QueryClientProvider>
   </StrictMode>,
 )
